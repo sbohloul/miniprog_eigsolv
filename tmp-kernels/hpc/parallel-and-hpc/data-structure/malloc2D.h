2e9713ee3ea05d47405a79a5e7de121fe7e335f9
Saeed Bohloul <sbohloul@gmail.com>
2023-07-07 12:25:58 -0400

    Clean up temp-kernels

--- tmp-kernels/hpc/parallel-and-hpc/data-structure/malloc2D.h
+++
@@ -1,4 +0,0 @@
-#ifndef MALLOC2D_H
-#define MALLOC2D_H
-double **malloc2D(int jmax, int imax);
-#endif
--- tmp-kernels/hpc/parallel-and-hpc/memory-bandwidth/plot-mem.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-fname = 'time.dat'
-df = pd.read_csv(fname)
-
-print(df)
