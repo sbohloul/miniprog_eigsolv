173ed19e974bad019a7862999acec718648b1be3
Saeed Bohloul <sbohloul@gmail.com>
2023-08-25 08:55:32 -0700

    Add setenv files for cedar
    - intel stack
    - gcc/ompi/netlibsca/flexiblas

---
+++ benchmarks/computecanada-cedar/setenv-gcc.sh
@@ -0,0 +1,8 @@
+module --force purge
+module load StdEnv/2020
+module load gcc/11.3.0
+module load openmpi/4.1.4
+module load scalapack/2.2.0
+module load mpi4py/3.1.4
+module load python/3.11.2
+module load cmake/3.23.1
---
+++ benchmarks/computecanada-cedar/setenv-intel.sh
@@ -0,0 +1,9 @@
+module --force purge
+module load StdEnv/2020  
+module load intel/2020.1.217  
+module load intelmpi/2019.7.217
+module load imkl/2022.1.0
+module load python/3.8
+module load mpi4py/3.0.3
+module load cmake/3.23.1
+
