56f30c55a1901ddd7ae3758120641b61345e1b73
Saeed Bohloul <sbohloul@gmail.com>
2023-09-13 13:22:21 -0700

    Add benchmark data for cedar
    - Add gcc-ompi-sca-oblas-o2 benchmark data

---
+++ benchmarks/computecanada-cedar/gcc-ompi-sca-oblas-o2/bin/scalapack/pdsyev/run_time_vs_gridshape.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DATADIR=/scratch/sbohloul/benchminiapp/data
+
+export M=4992
+export FNAME=$DATADIR/Si_${M}_HR.h5
+export NITER=1
+export MB=128
+export NB=128
+
+export NNODE=4
+export PPR=48
+export NP=$(($NNODE * $PPR))
+export NTHREADS=1
+export RESULTSDIR=tvsgs-si-${M}-n$NNODE-np$NP-nt$NTHREADS
+
+export OPENBLAS_NUM_THREADS=$NTHREADS
+export OMP_NUM_THREADS=$NTHREADS
+
+mpiexec -np $NP --map-by ppr:$PPR:node:pe=$NTHREADS --report-bindings python time_vs_gridshape.py $NITER $MB $NB $FNAME | tee log.out
+
+mkdir $RESULTSDIR
+lscpu | tee lscpu.log
+mv *.svg *.csv *.out *.log $RESULTSDIR
